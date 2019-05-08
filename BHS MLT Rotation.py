ccc84ed1258b6b95f9219ce67f6df0374d02eb0e
Charlie England <charlie265@live.com>
2019-05-08 13:35:31 -0700

    csv file change

--- BHS MLT Rotation.py
+++ BHS MLT Rotation.py
@@ -10,6 +10,9 @@ def chooserand():
 
 
 def check_day(randint, day):
+    """
+    checks the day to make sure that the provider can be assigned as MLT that day
+    """
     if day in providers[prov_list[randint]]:
         return True
     else:
@@ -17,47 +20,51 @@ def check_day(randint, day):
 
 
 def check_list(rand, count, rev, prov_list, bhaurgentlist):
+    """
+    this takes 5 arguments and checks to see if the person can be assigned by looking to see if they have been assigned in the past
+    based on the count (number of people currently in the list) it makes a decision whether or not to go back by a max of 4 for count in current list or by the reverse num if >4
+    if a person shows up, move forward check
+    """
     mv_fwrd_chk = 0
     if count == 1:
         return True
     elif count < 4:
         for num in range(count):
             if prov_list[rand] == bhaurgentlist[(len(bhaurgentlist)-1)-num]:
-                mv_fwrd_chk += 1
+                return False
     else:
         for num in range(rev):
             if prov_list[rand] == bhaurgentlist[(len(bhaurgentlist)-1)-num]:
-                mv_fwrd_chk += 1
-    if mv_fwrd_chk == 0:
-        return True
-    else:
-        return False
+                return False
+    return True
 
+
+def prov_lst_dict():
+    rtrn_dict = {}
+    for name in prov_list:
+        rtrn_dict.update({name:0})
+    return rtrn_dict
 
 def run_list_randomizer():
     bhaurgentlist = []
-    num_assigned = {"Brenda": 0, "Jody": 0, "Jarim": 0, "Katrina": 0, "Mike": 0,
-                    "Susan": 0, "Margaret": 0, "Dael": 0, "Lynette": 0}
+    num_assigned = prov_lst_dict()
     day = 1
     count = 1
-    rev = int(len(providers)*(.5))
+    rev_len = int(len(providers)*(.5)) #reverse length, the amount of days before a provider can be selected again. Based on 1/2 the number of providers, 6 providers = 3 days
     move_fwrd_day = False
     move_fwrd_list = False
     rand = 0
-    equitable = True
     run_number = 200
-    while count < run_number and equitable:
+    while count < run_number:
         rand = chooserand()
         move_fwrd_day = check_day(rand, day)
         if move_fwrd_day:
             move_fwrd_list = check_list(
-                rand, count, rev, prov_list, bhaurgentlist)
+                rand, count, rev_len, prov_list, bhaurgentlist)
             if move_fwrd_list:
                 count += 1
                 bhaurgentlist.append(prov_list[rand])
                 num_assigned[prov_list[rand]] += 1
-                if num_assigned[prov_list[rand]] > run_number/len(prov_list) + 2:
-                    equitable = False
                 day += 1
                 if day > 5:
                     day = 1
@@ -65,9 +72,9 @@ def run_list_randomizer():
 
 
 test_list = run_list_randomizer()
-while max(test_list[1].values()) - min(test_list[1].values()) > 2:
+while max(test_list[1].values()) - min(test_list[1].values()) > 2: #equitability check, looks at max and min and keeps calling list randomizer until everyone is within '2'
     test_list = run_list_randomizer()
-outfile = open("bhamltlist.csv", "w")
-for nm in test_list:
-    outfile.write("{},".format(nm))
-outfile.close()
+with open("bhamltlist.csv", "w") as of:
+    for nm in test_list[0]:
+        of.write(f"{nm},")
+ 
